565202249e0c35608318cd24937ccceaba00babe
richardcantareiro-web <richard.cantareiro@escola.pr.gov.br>
2026-06-24 12:31:44 -0300

    Criação de conexão co arquivo local JSON

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const form = Document.getElementById('form')
 const Input = document.getElementsByClassName('input')
 const todoUL = document.getElementById('todo')
+
+const todos = JSON.parse(localStorage.getItem('todos'))
+
